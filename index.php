1b1542401a5cb8f83191ab550b27a8903249f087
mitesh-123 <mitesh93official@gmail.com>
2026-01-21 15:39:48 +0530

    docker

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo "GYMS BACKEND ROOT WORKING";
+require __DIR__ . '/cors.php';
+
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($path === '/login') {
+    require __DIR__ . '/api/login.php';
+    exit;
+}
+
+echo json_encode(["status" => "API Running"]);
